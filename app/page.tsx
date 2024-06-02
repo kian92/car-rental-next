f5cfeed06a5f61435e9d3d90c4dfc2d5d69e6a45
Wei Kian <weikian92@hotmail.com>
2024-06-02 10:33:43 +0800

    hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import Hero from "@/components/Home/Hero";
 
 
 export default function Home() {
   return (
-    <h2>hello</h2>
+    <div className="p-5 sm:px-10 md:px-20">
+      <Hero />
+    </div>
+    
   );
 }
