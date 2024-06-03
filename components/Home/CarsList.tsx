20c5c5aee12ac48f82b02244c72223c497f17fe7
Wei Kian <weikian92@hotmail.com>
2024-06-03 19:20:18 +0800

    add modal ui

--- components/Home/CarsList.tsx
+++ components/Home/CarsList.tsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CarCard from './CarCard'
+import BookingModal from '../CarBooking/BookingModal'
 
 const CarsList = (carListArr: any) => {
-
+  const [selectedCar, setSelectedCar] = useState([]);
   return (
-    <div className='grid grid-cols-2 
-    md:grid-cols-3
-    lg:grid-cols-4'>
+    <div 
+    className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
         {carListArr.carlist.map((car:any, index: number) => (
-        <div key={index}>
+        <div key={index} onClick={()=>{(window as any).my_modal_4.showModal();
+          setSelectedCar(car)}}>
             <CarCard car={car} />
         </div>
-        
-    ))}</div>
+    ))}
+      <dialog id="my_modal_4" className="modal">
+        <BookingModal car={selectedCar}/>
+      </dialog>
+    </div>
   )
 }
 
