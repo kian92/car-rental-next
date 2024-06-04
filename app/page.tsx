f5f270b663566e4a6816449067d8f0c6aacce702
Wei Kian <weikian92@hotmail.com>
2024-06-04 16:40:16 +0800

    remove SearchInput component

--- app/page.tsx
+++ app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
     <div className="p-5 sm:px-10 md:px-20">
       <BookCreatedFlagContext.Provider value={{showToastMsg, setShowToastMsg}}>
         <Hero />
-        <SearchInput />
+        {/* <SearchInput /> */}
         <CarsFiltersOption 
           carlist={carsOrgList} 
           setBrand={(value: string) => filterCarList(value)}
